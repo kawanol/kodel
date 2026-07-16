4ff823479e62a3c08ef0d801beb32fca3e300624
kawanol <kawano.yuniv@gmail.com>
2026-07-16 16:38:43 +0900

    Create news.js

---
+++ news/news.js
@@ -0,0 +1,26 @@
+const newsItems = [
+  {
+    date: "2026/04/20",
+    datetime: "2026-04-20",
+    title: "研究室のホームページを開設しました",
+    url: "news/20260420-website-open.html"
+  },
+  {
+    date: "2026/04/08",
+    datetime: "2026-04-08",
+    title: "新メンバーが配属されました",
+    url: "news/20260408-new-members.html"
+  },
+  {
+    date: "2026/04/02",
+    datetime: "2026-04-02",
+    title: "研究室の看板をいただきました",
+    url: "news/20260402-lab-signboard.html"
+  },
+  {
+    date: "2026/04/01",
+    datetime: "2026-04-01",
+    title: "研究室が発足しました",
+    url: "news/20260401-lab-start.html"
+  }
+];
